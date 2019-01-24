7e5d610deee959e92b7f8d063302a14b36f07b82
swapnilhedau <swapnilhedau@192.168.0.15>
2019-01-24 13:49:47 -0500

    First commit for hello world java maven project

---
+++ helloworldjava/src/main/java/com/hedau/swapnil/helloworldjava/App.java
@@ -0,0 +1,13 @@
+package com.hedau.swapnil.helloworldjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
